bdd3e64d431c3f2b90dd77d91daa463de74ffa82
RobertoCarrichi <robertocarrichiunam@gmail.com>
2020-10-28 06:09:55 -0600

    "Cambié los nombres de algunos nombres para que se entendiera mejor c:"

---
+++ proyectNetBeans/src/proyectoNetBeans/Grupo.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Grupo{
+	// Atributos de instancia
+	private int clave;
+	Asignatura asignatura = new Asignatura();
+	Profesor profesor = new Profesor();
+}
