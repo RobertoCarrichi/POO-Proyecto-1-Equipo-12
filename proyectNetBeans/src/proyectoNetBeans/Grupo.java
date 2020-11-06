8918cba26f01814b69597adba4c6e0342ae8152c
RobertoCarrichi <robertocarrichiunam@gmail.com>
2020-11-05 19:58:43 -0600

    Se está organizando el proyecto final.

--- proyectNetBeans/src/proyectoNetBeans/Grupo.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-public class Grupo{
-	// Atributos de instancia
-	private int clave;
-	Asignatura asignatura = new Asignatura();
-	Profesor profesor = new Profesor();
-}
